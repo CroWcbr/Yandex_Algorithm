b8372da6b786357b297a610600e35eb8c537df21
Vitaliy Shavnin <crowf4i@gmail.com>
2024-03-09 21:37:11 +0100

    update

--- Yandex_5.0/00_training/h.cpp
+++ Yandex_5.0/00_training/h.cpp
@@ -1,27 +1,53 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
-double min_time(int L, int x1, double v1, int x2, double v2) 
+double t_one_point(int L, double x1, double v1, double x2, double v2)
 {
-    if (v1 == 0 && v2 == 0)
-        return (x1 == x2 || x1 + x2 == L) ? 0 : -1;
+    double dv = fabs(v2 - v1);
+    if (v1 == v2)
+        return -1;
+    else if (v1 > v2)
+        return x1 > x2 ? ((L - x1 + x2) / dv) : ((x2 - x1) / dv);
+    else //if (v2 > v1)
+        return x1 < x2 ? ((L - x2 + x1) / dv) : ((x1 - x2) / dv);  
+}
 
-    double dt = 0.00001;
-    double t = 0;
-    double d1 = x1;
-    double d2 = x2;
-    // while (fabs(d1 - d2) > 1e-9 || fabs(fabs(d1 + d2) - L) < 1e-9)
-    for (;fabs(d1 - d2) > 1e-9 || fabs(fabs(d1 + d2) - L) < 1e-9;t += dt)
-    {
-        std::cout << t << "\t" << d1 << "\t" << d2 << std::endl;
-        d1 = fmod(x1 + v1 * t, L);
-        if (d1 < 0)
-            d1 += L;
-        d2 = fmod(x2 + v2 * t, L);
-        if (d2 < 0)
-            d2 += L;  
-    }
-    return t;
+double t_two_point(int L, double x1, double v1, double x2, double v2)
+{
+    double tmin = std::numeric_limits<double>::infinity();
+    double t0 = (0 - x1 - x2) / (v1 + v2);
+    double t1 = (L - x1 - x2) / (v1 + v2);
+    double t2 = (2 * L - x1 - x2) / (v1 + v2);
+    if (t0 >= 0 && t0 < tmin)
+        tmin = t0;
+    if (t1 >= 0 && t1 < tmin)
+        tmin = t1;
+    if (t2 >= 0 && t2 < tmin)
+        tmin = t2;
+    
+    return tmin == std::numeric_limits<double>::infinity() ? -1 : tmin;
+}
+
+double min_time(int L, int x1, int v1, int x2, int v2) 
+{
+    if (x1 == x2 || x1 + x2 == L)
+        return 0;
+    if (v1 == 0 && v2 == 0)
+        return -1;
+
+    double t_one = t_one_point(L, x1, v1, x2, v2);
+    // std::cout << t_one << std::endl;
+
+    double t_two= t_two_point(L, x1, v1, x2, v2);
+    // std::cout << t_two << std::endl;
+
+    if (t_one == -1)
+        return t_two;
+    else if (t_two == -1)
+        return t_one;
+    else
+        return std::min(t_one, t_two);
 }
 
 int main() {
@@ -29,9 +55,11 @@ int main() {
     std::cin >> L >> x1 >> v1 >> x2 >> v2;
     
     double t = min_time(L, x1, v1, x2, v2);
+    // double t = min_time(12, 8, 10, 5, 20);
+    // double t = min_time(6, 3, 1, 1, 1);
     if (t < 0)
         std::cout << "no" << std::endl;
     else
-        std::cout << "yes" << std::endl << min_time(L, x1, v1, x2, v2) << std::endl;
+        std::cout << "yes" << std::endl << std::setprecision(10) << t << std::endl;
     return 0;
 }
