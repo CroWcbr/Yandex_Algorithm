5c04cf42fcf36e945b2929ccf2a10eaa5db1ac6a
Vitaliy Shavnin <crowf4i@gmail.com>
2024-03-28 16:56:44 +0100

    update

--- Yandex_5.0/03_binary_search/h_TL.cpp
+++ Yandex_5.0/03_binary_search/h_TL.cpp
@@ -1,70 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 #include <limits>
-#include <algorithm>
 
-#define DEBUG 0
-struct Party
-{
-    int vote;
-    int gift;
-    int id;
-};
+#define DEBUG 1
 
-bool operator<(const Party & p1, const Party & p2)
+bool check(int64_t mid, const std::vector<int>& data_vote, int choice_id)
 {
-	if (p1.vote < p2.vote)
-		return true;
-	else if (p1.vote == p2.vote && p1.gift < p2.gift)
-	    return true;
-	return false;
-}
-
-bool check(int64_t mid, std::vector<Party>& party, int choice_id)
-{
-    int64_t target_choice = party[choice_id].vote + mid;
-    for (int i = 0; i < party.size(); ++i)
+    int64_t target_choice = data_vote[choice_id] + mid;
+    for (int i = 0; i < data_vote.size(); ++i)
     {
-        if (i == choice_id || party[i].vote < target_choice)
+        if (i == choice_id || data_vote[i] < target_choice)
             continue;
         
-        mid = mid - (party[i].vote - target_choice + 1);
+        mid = mid - (data_vote[i] - target_choice + 1);
 
         if (mid < 0)
             break;
     }
-if (DEBUG) std::cout << "mid : " << mid << std::endl;
+
     if (mid >= 0)
         return true;
     else
         return false;
 }
 
-void binary_search(std::vector<Party> party,
+void binary_search(const std::vector<int>& data_vote, 
+                    const std::vector<int>& data_gift,
                     int64_t max_vote,
                     int& choice_id,
                     int64_t& additional_vote) 
 {
-    std::sort(party.begin(), party.end());
-    // for (int i = 0; i < party.size(); ++i)
-    //     std::cout << party[i].vote << "(" << party[i].gift << ") ";
-    // std::cout << std::endl;
-
-    int64_t answer = max_vote + 1;
-    for (int i = 0; i < party.size(); ++i)
+    int64_t answer = std::numeric_limits<int64_t>::max();
+    for (int i = 0; i < data_vote.size(); ++i)
     {
-        if (party[i].gift == -1)
+        if (data_gift[i] == -1)
             continue;
-
-        if (choice_id != -1)
-        {
-            int tmp_additional = answer - party[i].gift;
-            if (tmp_additional < 0)
-                continue;
-            if (party[i].vote + tmp_additional <= party[choice_id].vote + additional_vote)
-                continue;
-        }
 
         int64_t L = 0;
         int64_t R = max_vote;
@@ -72,50 +44,17 @@ void binary_search(std::vector<Party> party,
         while (L < R)
         {
             int64_t mid = (L + R) / 2;
-if (DEBUG) std::cout << mid << " " << L << " " << R << std::endl;
-            if (check(mid, party, i))
+            if (check(mid, data_vote, i))
                 R = mid;
             else
                 L = mid + 1;
-if (DEBUG) std::cout << "\t" << mid << " " << L << " " << R << std::endl;
         }
-        if (answer > L + party[i].gift)
+
+        if (answer > L + data_gift[i])
         {
-            answer = L + party[i].gift;
+            answer = L + data_gift[i];
             choice_id = i;
             additional_vote = L;
-        }
-    }
-    choice_id = party[choice_id].id;
-}
-
-void update_data_vote(std::vector<Party>& party, int additional_vote, int choice_id)
-{
-    bool flag = false;
-    party[choice_id].vote += additional_vote;
-    for (int i = 0, n = party.size(); i < n, additional_vote; ++i)
-    {
-        if (i == choice_id)
-            ;
-        else if (flag && party[i].vote > 0)
-        {
-            party[i].vote--;
-            additional_vote--;
-        }
-        else if (party[choice_id].vote > party[i].vote)
-            ;
-        else
-        {
-            while (party[choice_id].vote <= party[i].vote)
-            {
-                party[i].vote--;
-                additional_vote--;
-            }
-        }
-        if (i == n - 1)
-        {
-            flag = true;
-            i = -1;
         }
     }
 }
@@ -125,25 +64,54 @@ int main()
     std::ifstream input("input.txt");
     int n;
     input >> n;
-    std::vector<Party> party(n);
+
+    std::vector<int> data_vote(n);
+    std::vector<int> data_gift(n);
     int64_t max_vote = 0;
     for (int i = 0; i < n; ++i)
     {
-        input >> party[i].vote >> party[i].gift;
-        max_vote += party[i].vote;
-        party[i].id = i;
+        input >> data_vote[i] >> data_gift[i];
+        max_vote += data_vote[i];
     }
     input.close();
 
-    int choice_id = -1;
-    int64_t additional_vote = 0;
-    binary_search(party, max_vote, choice_id, additional_vote);
 
-    std::cout << additional_vote + party[choice_id].gift << std::endl;
+    int choice_id;
+    int64_t additional_vote;
+    binary_search(data_vote, data_gift, max_vote, choice_id, additional_vote);
+
+    std::cout << additional_vote + data_gift[choice_id] << std::endl;
     std::cout << choice_id + 1 << std::endl;
-    update_data_vote(party, additional_vote, choice_id);
+    bool flag = false;
+    data_vote[choice_id] += additional_vote;
+    int i = 0;
+    for (; i < n, additional_vote; ++i)
+    {
+        if (i == choice_id)
+            ;
+        else if (flag && data_vote[i] > 0)
+        {
+            data_vote[i]--;
+            additional_vote--;
+        }
+        else if (data_vote[choice_id] > data_vote[i])
+            ;
+        else
+        {
+            while (data_vote[choice_id] <= data_vote[i])
+            {
+                data_vote[i]--;
+                additional_vote--;
+            }
+        }
+        if (i == n - 1)
+        {
+            flag = true;
+            i = -1;
+        }
+    }
     for (int i = 0; i < n; ++i)
-        std::cout << party[i].vote << " ";
+        std::cout << data_vote[i] << " ";
     std::cout << std::endl;
     return 0;
 }
